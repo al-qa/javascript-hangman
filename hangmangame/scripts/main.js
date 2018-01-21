1a40a695ca66c2e8158ac79a28f78ca7540be4ff
24HourDeveloper <grownmanherbal@gmail.com>
2018-01-21 00:24:11 -0500

    The code is at the bootom of the main js commented out.

--- hangmangame/scripts/main.js
+++ hangmangame/scripts/main.js
@@ -1,50 +1,46 @@
 var guess = 0;
+var secretWords = ["halo", "cod", "madden"];
 
 function validateInput()
 {
 var letter = document.getElementById("letter").value;
-<!-- document.getElementById("guess").innerHTML=""; -->
+//<!-- document.getElementById("guess").innerHTML=""; -->
 
 
   while(guess < 6){
     if (letter == "h")
     {
-    document.getElementById("h").innerHTML="H";
-    return;
+    return document.getElementById("h").innerHTML="H";
     }
     if (letter == "a")
     {
-    document.getElementById("a").innerHTML="A";
-    return;
+    return document.getElementById("a").innerHTML="A";
     }
     if (letter == "l")
     {
-    document.getElementById("l").innerHTML="L";
-    return;
+    return document.getElementById("l").innerHTML="L";
     }
     if (letter == "o")
     {
-    document.getElementById("o").innerHTML="O";
-    return;
+    return document.getElementById("o").innerHTML="O";
     }
-
 
     guess++;
 
+
     if(guess == 6)
     {
-      var ulose = document.createElement("h1");
-      var node = document.createTextNode("YOU LOSE!!");
-      ulose.appendChild(node);
-      var element = document.getElementById('youLose');
-      element.appendChild(ulose);
+      var youLoseh1 = document.createElement("h1");
+      var youLoseNode = document.createTextNode("YOU LOSE!!");
+      youLoseh1.appendChild(youLoseNode);
+      var youLoseDiv = document.getElementById('youLose');
+      youLoseDiv.appendChild(youLoseh1);
     }
 
     updateImage();
     return;
   }
 }
-
 
 
 function updateImage()
@@ -60,3 +56,54 @@ function updateImage()
   }
 return;
 }
+
+
+/*
+//This code in not complete! It counts 6 times even if you get an answer correct
+//It set to check ehat ever input has been selected and check it against the secretWord
+//its not been setup to the code as far as showing in the html yet
+//I'm only trying to get the logic together for now did add it
+
+var secretWords = ["halo", "cod", "madden"];
+var random = Math.random()*2;
+
+var round = Math.round(random);
+var selectedWord =  "halo";  //secretWords[round];
+
+//var guess = "h";
+var letter;
+var input = "";
+
+var count = 0;
+
+while(count < 6){
+   letter = prompt("Enter letter: ");
+
+   checkGuess(letter);
+   if(input == selectedWord){
+      console.log("You Win!!");
+   }
+   count++;
+
+   wrongAnswers(count);
+}
+
+function wrongAnswers(chance){
+   if(chance == 6){
+      return console.log("You lose");
+   }
+}
+
+function checkGuess(guess){
+      var n = selectedWord.indexOf(guess);
+
+      if(n == -1){
+         return console.log("wrong");
+      }else{
+         input += selectedWord.charAt(n);
+         return console.log(input);
+      }
+}
+
+
+*/
